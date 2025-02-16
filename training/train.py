a5e692f20ff9f98f860b4eefa2c62152a8afeef4
armus11 <armanmusaev221@gmail.com>
2025-02-16 11:58:31 +0500

    first commit

--- training/train.py
+++ training/train.py
@@ -24,7 +24,8 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(ROOT_DIR))
 
 # Change to CONF_FILE = "settings.json" if you have problems with env variables
-CONF_FILE = os.getenv('CONF_PATH') 
+# CONF_FILE = os.getenv('CONF_PATH') 
+CONF_FILE = "settings.json" 
 
 from utils import get_project_dir, configure_logging
 
